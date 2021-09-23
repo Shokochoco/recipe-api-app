cc03328d6ad9100f65ce9f96a02196c7a2c5d117
Shoko <c.sh0k0.mode@gmail.com>
2021-09-23 15:12:34 +0900

    Initial Commit

---
+++ recipe-api-app/RecipeData.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeData.swift
+//  recipe-api-app
+//
+//  Created by 鈴木淳子 on 2021/09/22.
+//
+
+import Foundation
---
+++ recipe-api-app/RecipeManager.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeManager.swift
+//  recipe-api-app
+//
+//  Created by 鈴木淳子 on 2021/09/19.
+//
+
+import Foundation
